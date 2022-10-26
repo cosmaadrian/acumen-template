47bd9ec6db20cf448310d7f962b9377d96300fa5
Adrian Cosma <cosma.i.adrian@gmail.com>
2022-10-26 16:02:34 +0200

    fixed bug in dataset

--- {{cookiecutter.project_slug}}/lib/dataset_extra/acumen_dataset.py
+++ {{cookiecutter.project_slug}}/lib/dataset_extra/acumen_dataset.py
@@ -3,10 +3,10 @@ from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
 
 class AcumenDataset(Dataset):
-	def __init__(self, args, kind = 'train', data_transforms = None):
-		self.args = args
-		self.kind = kind
-		self.transforms = transforms
+    def __init__(self, args, kind = 'train', data_transforms = None):
+        self.args = args
+        self.kind = kind
+        self.transforms = transforms
 
     @classmethod
     def train_dataloader(cls, args, annotations = None):
