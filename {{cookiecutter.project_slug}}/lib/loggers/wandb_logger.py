fca5370a64062c9d55791715af41068f9a5cf79e
Adrian Cosma <cosma.i.adrian@gmail.com>
2022-10-26 18:58:24 +0200

    fixed model loading, fixed wandb_logger

--- {{cookiecutter.project_slug}}/lib/loggers/wandb_logger.py
+++ {{cookiecutter.project_slug}}/lib/loggers/wandb_logger.py
@@ -10,6 +10,16 @@ class WandbLogger(object):
     def watch(self, model):
         wandb.watch(model)
 
+    def log_dict(self, log_dict, on_step = True, force_log = False):
+        for key, value in log_dict.items():
+            self.metrics[key] = value
+            if on_step:
+                self.on_step_metrics[key] = value
+
+        if (self.trainer.global_step % self.trainer.args.log_every == 0) or force_log:
+            if wandb.run is not None:
+                wandb.log(log_dict, step = self.trainer.global_step)
+
     def log(self, key, value, on_step = True, force_log = False):
         self.metrics[key] = value
 
@@ -17,6 +27,6 @@ class WandbLogger(object):
             self.on_step_metrics[key] = value
 
         if (self.trainer.global_step % self.trainer.args.log_every == 0) or force_log:
-            wandb.log({key: value}, step = self.trainer.global_step)
-            return
+            if wandb.run is not None:
+                wandb.log({key: value}, step = self.trainer.global_step)
 
