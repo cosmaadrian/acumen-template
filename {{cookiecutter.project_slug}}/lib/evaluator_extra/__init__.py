152fa42d45d2ff7ee7fdec630a83ab3a814ea832
Adrian Cosma <43779669+cosmaadrian@users.noreply.github.com>
2024-04-10 14:23:41 +0300

    Update __init__.py

--- {{cookiecutter.project_slug}}/lib/evaluator_extra/__init__.py
+++ {{cookiecutter.project_slug}}/lib/evaluator_extra/__init__.py
@@ -1,3 +1,4 @@
 from .acumen_evaluator import AcumenEvaluator
 from .acumen_metrics import Metric, MetricCollection
 from .classification_evaluator import AcumenClassificationEvaluator
+from .evaluation_aggregator import AcumenEvaluationAggregator
