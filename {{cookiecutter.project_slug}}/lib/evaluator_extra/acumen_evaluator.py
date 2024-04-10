3da81335584f6b1804f38f93e0fa934b567a9e96
Adrian Cosma <43779669+cosmaadrian@users.noreply.github.com>
2024-04-10 14:23:53 +0300

    Update acumen_evaluator.py

--- {{cookiecutter.project_slug}}/lib/evaluator_extra/acumen_evaluator.py
+++ {{cookiecutter.project_slug}}/lib/evaluator_extra/acumen_evaluator.py
@@ -11,9 +11,12 @@ class AcumenEvaluator(object):
         self.evaluator_args = evaluator_args
         self.trainer = None
         self._logger = logger
+        self.is_aggregator = False
 
         if 'output_dir' in self.args and self.args.output_dir is not None:
             os.makedirs(f'results/{self.args.output_dir}', exist_ok = True)
+
+        self.current_metric_collection = []
 
     @property
     def display_name(self):
@@ -35,24 +38,31 @@ class AcumenEvaluator(object):
         # returns a dictionary or a list of dicts or a list of metrics or just a metric
         raise NotImplementedError
 
-    def evaluate_and_log(self, global_step = -1):
+    @torch.no_grad()
+    def evaluate_and_log(self, global_step = -1, metrics = None):
+        if metrics is not None:
+            metrics.log(self.logger)
+            return
+
         outputs = self.trainer_evaluate(global_step)
 
-        metric_collection = MetricCollection(evaluator = self)
+        metric_collection = MetricCollection()
 
         if isinstance(outputs, dict):
             for key, value in outputs.items():
-                metric_collection.append(Metric(name = key, value = value))
+                metric_collection.append(Metric(name = key, evaluator = self, value = value))
 
         elif isinstance(outputs, list):
             for output in outputs:
                 if isinstance(output, dict):
                     for key, value in output.items():
-                        metric_collection.append(Metric(name = key, value = value))
+                        metric_collection.append(Metric(name = key, evaluator = self, value = value))
                 else:
                     metric_collection.append(output)
 
         elif isinstance(outputs, Metric):
             metric_collection.append(outputs)
+
+        self.current_metric_collection = metric_collection
 
         metric_collection.log(self.logger)
