829b3369d2061466f57c1aa5b8893d155748ccb3
Adrian Cosma <43779669+cosmaadrian@users.noreply.github.com>
2024-04-10 14:24:03 +0300

    Update acumen_metrics.py

--- {{cookiecutter.project_slug}}/lib/evaluator_extra/acumen_metrics.py
+++ {{cookiecutter.project_slug}}/lib/evaluator_extra/acumen_metrics.py
@@ -2,25 +2,35 @@ INCREASING_METRICS = ['accuracy', 'f1', 'precision', 'recall']
 DECREASING_METRICS = ['loss', 'error', 'mae', 'mse']
 
 class MetricCollection(object):
-    def __init__(self, evaluator, metrics = None):
+    def __init__(self, metrics = None):
         self.metrics = metrics
-        self.evaluator = evaluator
+        self.index = 0
 
         if metrics is None:
             self.metrics = []
+
     def append(self, metric):
         self.metrics.append(metric)
 
+    def extend(self, metric_collection):
+        for m in metric_collection:
+            self.metrics.append(m)
+
     def log(self, logger):
         for metric in self.metrics:
-            # this sucks, but oh well
-            metric.log(logger, evaluator = self.evaluator)
+            metric.log(logger)
+
+    def __iter__(self):
+        return self.metrics.__iter__()
 
 class Metric(object):
-    def __init__(self, name, value, monotonicity = None):
+    def __init__(self, name, value, evaluator = None, monotonicity = None):
         self.name = name
         self.value = value
         self.monotonicity = monotonicity
+        self.evaluator = evaluator
+
+        self.log_name =  f'{self.evaluator.display_name}#{self.name}'
 
         if monotonicity is None:
             if any([metric in self.name for metric in INCREASING_METRICS]):
@@ -32,7 +42,7 @@ class Metric(object):
             else:
                 self.monotonicity = ['instant']
 
-    def log(self, logger, evaluator):
+    def log(self, logger):
         log_min = False
         log_max = False
         log_instant = False
@@ -47,7 +57,7 @@ class Metric(object):
             log_instant = True
 
         logger.log(
-            f'{evaluator.display_name}#{self.name}',
+            self.log_name,
             self.value,
             on_step = False,
             force_log = True,
